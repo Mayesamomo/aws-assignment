fccc4784eec15de944c8e197a860729d06206a11
Michael A. Momo <mayesamomo1997@gmail.com>
2021-02-10 14:36:22 +0000

    userdata.sh added

--- userdata.sh
+++ userdata.sh
@@ -7,8 +7,31 @@ sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-#sudo yum install wget
+sudo groupadd www
+sudo usermod -a -G www ec2-user
 
+#exit and log back in and verify www group exists with the groups command by typing "groups"
+#Change the group ownership of the /var/www directory and its contents to the www group.
+sudo chgrp -R www /var/www
+
+#Change the directory permissions of /var/www and its subdirectories to add group write permissions and set the group ID on subdirectories created in the future.
+
+#sudo su as a root user
+sudo su 
+chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} +
+
+#change the permissions for files in the /var/www directory and its subdirectories to add group write permissions.
+
+find /var/www -type f -exec sudo chmod 0664 {} +
+
+# change the directory to /var/www and create a new subdirectory named inc.
+
+cd /var/www
+mkdir inc
+cd inc
+#Create a new file in the inc directory named dbinfo.inc, and then edit the file and enter database set-up config .
+nano dbinfo.inc
 #Install Node
 #nvm install node
 
